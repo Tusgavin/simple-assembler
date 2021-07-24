9749f1c5346039772a3f546c08f8606098398685
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-07-24 12:03:34 -0300

    📦 NEW: Operation Constructor implemented

--- src/montador.cpp
+++ src/montador.cpp
@@ -30,9 +30,52 @@ Operation::Operation(std::string operation_stringfied)
    operation_stringfied = trim(operation_stringfied);
 
    // Operador
+   std::string whitespace_delimiter = " ";
+   std::size_t operator_end_index = operation_stringfied.find(whitespace_delimiter);
+   if (operator_end_index != std::string::npos)
+   {
+      std::string operador = operation_stringfied.substr(0, operator_end_index);
+      this->operador(operador);
+      operation_stringfied = operation_stringfied.substr(operator_end_index + 1);
+   }
+   else
+   {
+      if (operation_stringfied.length() != 0)
+         this->operador(operation_stringfied);
+         this->operando1("");
+         this->operando2("");
+         operation_stringfied = "";
+         return;
+   }
 
+   operation_stringfied = trim(operation_stringfied);
 
-   // Operando1
+   // Operando1 - first check if there is any Operando
+   if (operation_stringfied.length() != 0)
+   {
+      std::size_t operand1_end_index = operation_stringfied.find(whitespace_delimiter);
+      if (operator_end_index != std::string::npos)
+      {
+         std::string operando1 = operation_stringfied.substr(0, operand1_end_index);
+         this->operando1(operando1);
+         operation_stringfied = operation_stringfied.substr(operand1_end_index + 1);
+      }
+      else
+      {
+         if (operation_stringfied.length() != 0)
+            this->operando1(operation_stringfied);
+            this->operando2("");
+            operation_stringfied = "";
+            return;
+      }
+   }
 
-   // Operando2
+   operation_stringfied = trim(operation_stringfied);
+
+   // Operando2 - first check if there is any Operando
+   if (operation_stringfied.length() != 0)
+   {
+      this->operando2(operation_stringfied);
+      operation_stringfied = "";
+   }
 }
